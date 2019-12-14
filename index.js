aaae0ed3eef5da8b49a82ca900e3f28872cc877b
John Gerald Agbayani <john.agbayani@idearobin.com>
2019-12-14 13:10:36 +0800

    Add an output example

--- index.js
+++ index.js
@@ -17,31 +17,28 @@ const logOperation = (currentState, transition, resultingTape, head) => {
     const tapeState = getTapeRepresentation(resultingTape)
     const tapeHead = getTapeHeadRepresentation(transitionFunction.length, head)
 
-    console.log(tapeHead)
-    console.log(`${transitionFunction} \t ${tapeState}`)
+    console.log("    " + tapeHead)
+    console.log("    " + `${transitionFunction} \t ${tapeState}`)
     console.log()
 }
 
 const transitions = {
-    q0: [
-        ["H", "h", "R", "q0"],
-        ["e", "3", "R", "q0"],
-        ["l", "L", "R", "q0"],
-        ["o", "0", "R", "q0"],
-        [",", ",", "R", "q0"],
-        [" ", " ", "R", "q0"],
-        ["w", "w", "R", "q0"],
-        ["r", "r", "R", "q0"],
-        ["d", "d", "R", "q0"],
-        ["_", "!", "R", "q1"]
-    ],
-    q1: [],
+    q0: [["0", "0", "R", "q0"], ["1", "1", "R", "q0"], ["_", "$", "L", "q1"]],
+    q1: [["0", "0", "L", "q1"], ["1", "1", "L", "q1"], ["_", "_", "R", "q2"]],
+    q2: [["0", "0", "R", "q2"], ["1", "1", "R", "q3"]],
+    q3: [["Y", "Y", "R", "q3"], ["0", "Y", "L", "q4"], ["$", "_", "L", "q8"]],
+    q4: [["0", "0", "L", "q4"], ["1", "1", "L", "q4"], ["X", "X", "L", "q4"], ["Y", "Y", "L", "q4"], ["$", "$", "L", "q4"], ["_", "_", "R", "q5"]],
+    q5: [["X", "X", "R", "q5"], ["0", "X", "R", "q6"], ["1", "1", "L", "q7"]],
+    q6: [["0", "0", "R", "q6"], ["1", "1", "R", "q6"], ["Y", "Y", "R", "q6"], ["$", "$", "R", "q6"], ["_", "0", "L", "q4"]],
+    q7: [["X", "0", "L", "q7"], ["_", "_", "R", "q2"]],
+    q8: [["0", "_", "L", "q8"], ["1", "_", "L", "q8"], ["X", "_", "L", "q8"], ["Y", "_", "L", "q8"], ["_", "_", "R", "q9"]],
+    q9: []
 }
 
-const input = "Hello, world"
+const input = "0010"
 const blankSymbol = "_"
 const initialState = "q0"
-const finalStates = ["q1"]
+const finalStates = ["q9"]
 
 let currentState = initialState
 let tape = [blankSymbol, ...input.split(""), blankSymbol];
